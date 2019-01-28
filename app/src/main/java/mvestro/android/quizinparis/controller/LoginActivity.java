21c0f0c6c394ea2b45d74b673bf33a1cbb383466
MarwanJerbi <je.marwan@gmail.com>
2019-01-28 15:23:32 +0100

    'login'

--- app/src/main/java/mvestro/android/quizinparis/controller/LoginActivity.java
+++ app/src/main/java/mvestro/android/quizinparis/controller/LoginActivity.java
@@ -1,14 +1,21 @@
 package mvestro.android.quizinparis.controller;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 import mvestro.android.quizinparis.R;
 
@@ -53,11 +60,34 @@ public class LoginActivity extends AppCompatActivity {
         mValidateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText username = (EditText)findViewById(R.id.activity_login_id_input);
+                EditText password = (EditText)findViewById(R.id.activity_login_password_input);
+                remoteLogin(username.getText().toString(), password.getText().toString());
                     // User clicked the button
-                    Intent gridActivityIntent = new Intent(LoginActivity.this, GridActivity.class);
-                    startActivity(gridActivityIntent);
+                    //Intent gridActivityIntent = new Intent(LoginActivity.this, GridActivity.class);
+                    //startActivity(gridActivityIntent);
             }
         });
 
+    }
+
+    public void remoteLogin(String username, String password){
+        ProgressDialog pDialog = new ProgressDialog(this);
+        String urlString = "http://172.30.46.136/iot-server/api/login.php?key=iot1235&username="+username+"&password="+password;
+        Ion.with(this).load(urlString).asJsonObject().setCallback(new FutureCallback<JsonObject>() {
+            @Override
+            public void onCompleted(Exception e, JsonObject result) {
+                Log.d("test", result.toString());
+                if(result.get("id") != null){
+
+                    Friend me = (Friend) Friend.getFriendFromJson(result);
+                    Intent intent = new Intent(LoginActivity.this, GridActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "pas bon id", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
